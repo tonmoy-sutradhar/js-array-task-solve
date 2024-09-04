7ceb7eb32f37c34880bb911fc8b43314f814596a
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-04 11:05:17 +0600

    Declare an array of 3 tourist destinations and add two destinations and remove 2 destinations.

---
+++ task2.js
@@ -0,0 +1,20 @@
+// ** Add or remove elements
+// 1. Declare an array of 3 tourist destinations
+// 2. Add a new tourist destination to your tourist array
+// 3. Add two more to your array
+// 4. Remove the last tourist destination you have added
+// 5. display the final array as output
+
+let place = ["Maldives", "India", "America"];
+place.push("Nepal");
+
+console.log(place);
+
+place.push("Dubai");
+place.push("Bangladesh");
+
+console.log(place);
+
+place.pop();
+place.pop();
+console.log(place);
