2a04052b5d8dabe720b022d42041e393561d1ab3
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-04 14:56:57 +0600

    Combining array practice done.

---
+++ task5.js
@@ -0,0 +1,10 @@
+//  Combining Arrays
+// **Instructions:**
+// 1. Create two arrays of your choice.
+// 2. Use the concat method to combine the two arrays into a new array.
+// 3. Print both the original arrays and the combined array using console.log().
+let phones = ["Vivo", "Iphone", "LG", "Nokia"];
+let books = ["bnagla", "english", "math"];
+
+let join = phones.join(books);
+console.log(join + " ");
