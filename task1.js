d6b9eb987b7239fcd31f48d46a88060dcd7fb998
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-04 10:52:06 +0600

    Declare an array with 5 elements containing fruits and change the 2nd index practice done.

---
+++ task1.js
@@ -0,0 +1,11 @@
+//  1. Declare an array
+// 1. Declare an array with 5 elements containing fruits
+// 2. console log the 3rd index element
+// 3. change the value of the 2nd index element to jambura
+// 4. console log the final array
+
+let fruits = ["Apple", "Orange", "Banana", "Water-melon", "Strawberry"];
+console.log(fruits);
+console.log(fruits[3]);
+fruits[2] = "jambura";
+console.log(fruits);
